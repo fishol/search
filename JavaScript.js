d72dc10cd981a7a5520a0a03f9ae959df8958c40
熱帶魚 <o.david.yu@gmail.com>
2022-07-23 21:30:08 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function toggle(){
+      var sec = document.getElementById('sec');
+      var nav = document.getElementById('navigation');
+      sec.classList.toggle('active')
+      nav.classList.toggle('active')
+    }
